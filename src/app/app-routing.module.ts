8c7071ba058a1b4b8ab303677ea9a1272f9cc96f
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-11-03 15:55:40 -0700

    Terminado navegacion
    gg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CitasPendientesComponent } from './paginas/citas/citas-pendientes/citas-pendientes.component';
 import { RegistrarCitaComponent } from './paginas/citas/registrar-cita/registrar-cita.component';
+import { GestionRefaccionesComponent } from './paginas/gestion/gestion-refacciones/gestion-refacciones.component';
+import { GestionUsuariosComponent } from './paginas/gestion/gestion-usuarios/gestion-usuarios.component';
+import { HistorialServiciosComponent } from './paginas/historial/historial-servicios/historial-servicios.component';
 import { HomePageComponent } from './paginas/home-page.component';
+import { ServiciosPendientesComponent } from './paginas/servicios/servicios-pendientes/servicios-pendientes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -13,7 +17,7 @@ const routes: Routes = [
     component: HomePageComponent,
     children: [
       {
-        path: 'citas',
+        path: 'cita',
         children: [
           {
             path: '',
@@ -21,14 +25,42 @@ const routes: Routes = [
             pathMatch: 'full',
           },
           {
-            path: 'pendientes',
-            component: CitasPendientesComponent,
-          },
-          {
             path: 'registrar',
             component: RegistrarCitaComponent,
           },
+          {
+            path: 'pendientes',
+            component: CitasPendientesComponent,
+          },
         ],
+      },
+      {
+        path: 'taller',
+        children: [
+          {
+            path: '',
+            redirectTo: 'ingreso',
+            pathMatch: 'full',
+          },
+          {
+            path: 'serviciosPendientes',
+            component: ServiciosPendientesComponent,
+          },
+          {
+            path: 'historialServicio',
+            component: HistorialServiciosComponent,
+          },
+        ],
+      },
+      {
+        path: 'usuarios',
+        pathMatch: 'full',
+        component: GestionUsuariosComponent,
+      },
+      {
+        path: 'recursos',
+        pathMatch: 'full',
+        component: GestionRefaccionesComponent,
       },
     ],
   },
