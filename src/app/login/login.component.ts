cddc662eaef6b336716c64f5a71d36781addc19e
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-11-07 09:54:44 -0700

    algunos diseños
    gg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../servicios/login/login.service';
 
@@ -9,14 +9,25 @@ import { LoginService } from '../servicios/login/login.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  loginForm = new FormGroup({
-    CORREO: new FormControl(''),
-    CONTRA: new FormControl(''),
-  });
+  // loginForm = new FormGroup({
+  //   CORREO: new FormControl(''),
+  //   CONTRA: new FormControl(''),
+  // });
+
+  loginForm: any;
 
   response: string = '';
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      CORREO: '',
+      CONTRA: '',
+    });
+  }
 
   ngOnInit(): void {
     if (localStorage.getItem('TOKEN')) this.router.navigate(['/cita']);
@@ -26,16 +37,18 @@ export class LoginComponent implements OnInit {
     let correo = this.loginForm.value.CORREO;
     let contra = this.loginForm.value.CONTRA;
 
+    alert(correo + " " +contra);
+
     if (correo == '' || contra == '') {
       alert('Porfavor asegurese de introducir todos los campos requeridos');
       return;
     }
 
-    this.loginService.validateLogin(this.loginForm.value).subscribe(
-      (response: any) => {
+    this.loginService
+      .validateLogin(this.loginForm.value)
+      .subscribe((response: any) => {
         localStorage.setItem('TOKEN', response.TOKEN);
         this.router.navigate(['/cita']);
-      }
-    );
+      });
   }
 }
