048100466106cc195a6a077108b2d06fd7e3be17
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-11-13 22:58:56 -0700

    quedó recrusos

--- src/app/servicios/clientes/cliente.service.ts
+++ src/app/servicios/clientes/cliente.service.ts
@@ -8,24 +8,22 @@ import { environment } from 'src/environments/environment';
 })
 export class ClienteService {
   url = environment.baseUrlAPI;
-  headers = new HttpHeaders()
-            .set("Authorization", "Bearer "+localStorage.getItem("TOKEN"));
-  constructor(private http: HttpClient) { }
+  headers = new HttpHeaders().set(
+    'Authorization',
+    'Bearer ' + localStorage.getItem('TOKEN')
+  );
+  constructor(private http: HttpClient) {}
 
-  getUsuarioToken(){
-    return this
-            .http
-            .get(`${this.url}/usuarios/token`, {headers: this.headers}).pipe(
-              map(res=>res)
-            );
+  getUsuarioToken() {
+    return this.http
+      .get(`${this.url}/usuarios/token`, { headers: this.headers })
+      .pipe(map((res) => res));
   }
 
-  getClientes(){
-    return this
-            .http
-            .get(`${this.url}/usuarios/clientes`, {headers: this.headers}).pipe(
-              map(res=>res)
-            );
+  getClientes() {
+    return this.http
+      .get(`${this.url}/usuarios/clientes`, { headers: this.headers })
+      .pipe(map((res) => res));
   }
 
   registrarUsuario(form: any) {
@@ -40,7 +38,7 @@ export class ClienteService {
       .pipe(map((res) => res));
   }
 
-  updateUser(data:any,id:any){
+  updateUser(data: any, id: any) {
     return this.http.put(`${this.url}/usuarios/${id}`, data, {
       headers: this.headers,
     });
